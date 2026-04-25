8bba00142755aaf262bc8a32dd96db9cf5851235
annacastrom <a00841709@tec.mx>
2026-04-24 20:59:57 -0600

    Creación de archivos de componentes y estructura base de ContactoForm

---
+++ src/components/ContactoForm.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useState } from 'react'
+
+function ContactoForm({onAgregar, numero}) { // Solo recibe número para añadirlo en el subtítulo de contactos guardados
+    const [nombre, setNombre] = useState('');
+    const [telefono, setTelefono] = useState('');
+    const [categoria, setCategoria] = useState('Trabajo');
+    
+  return (
+    <div className='contactoform-contenedor'>
+        <h2 className='contactoform-titulo'>Mi agenda</h2>
+        <h3 className='contactoform-contactos-guardados'><span className='contactoform-contactos-numero'>{numero}</span> contactos guardados</h3>
+        
+        <input 
+            className='contactoform-nombre'
+            type="text"
+            value={nombre}
+            placeholder="Nombre completo"
+            onChange={(e) => setNombre(e.target.value)}>
+        </input>
+
+        <input 
+            className='contactoform-telefono'
+            type="text"
+            value={telefono}
+            placeholder="Teléfono"
+            onChange={(e) => setTelefono(e.target.value)}>
+        </input>
+
+        <select className='contactoform-categoria' value={categoria} onChange={(e) => setCategoria(e.target.value)}>
+            <option value="Trabajo">Trabajo</option>
+            <option value="Familia">Familia</option>
+            <option value="Amigo">Amigo</option>
+        </select>
+
+        <button 
+            className='contactoform-agregar-contacto' 
+            onClick={onAgregar}>
+            + Agregar contacto
+        </button>
+
+    </div>
+  )
+}
+
+export default ContactoForm
